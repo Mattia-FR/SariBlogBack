96dbc32d078f9219902eea10e999d7918ac0aa43
Mattia-FR <mattia.bouvier@protonmail.com>
2025-11-02 15:42:17 +0100

    Initialized.

---
+++ index.ts
@@ -0,0 +1,13 @@
+import app from "./src/app";
+import dotenv from "dotenv";
+
+dotenv.config();
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 4242;
+
+app.get("/", (req, res) => {
+    res.status(200).send("Je suis sur l'API 'http://localhost:4242'");
+})
+
+app.listen(port, () => {
+    console.log(`Server adress : http://localhost:${port}`);
+});
